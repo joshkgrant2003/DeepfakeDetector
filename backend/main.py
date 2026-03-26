37475d4ea81c505e88b20d8f56829f78892fd32b
Joshua Grant <joshkgrant2003@gmail.com>
2026-03-25 20:15:14 -0400

    update allowed origins

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ import io
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://deepfake-detector-theta.vercel.app/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
